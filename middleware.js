50f66d86e186d688c1a0f9a0f5ce676cb8dd7f7e
devern-DZZC <devern.chattergoon@my.uwi.edu>
2025-08-03 13:38:57 -0400

    location logging

---
+++ middleware.js
@@ -0,0 +1,15 @@
+import { NextResponse } from 'next/server';
+
+export function middleware(NextRequest) {
+  const geo = request.geo;
+
+  if (geo) {
+    const country = geo.country || 'unknown';
+    const region = geo.region || 'unknown';
+    const city = geo.city || 'unknown';
+
+    console.log(`Visitor from ${city}, ${region}, ${country}`);
+  }
+
+  return NextResponse.next();
+}
